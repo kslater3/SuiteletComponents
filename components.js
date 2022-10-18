7e625c2272b7b9f98853d2b010b55dacd7f4d3ac
kslater3 <kslater3@missouriwestern.edu>
2022-10-18 10:04:32 -0500

    Fixed Undefined Data Reference Errors

--- components.js
+++ components.js
@@ -15,81 +15,81 @@ class RecordsTable {
 
 
     _build_header(firstRow) {
-        let table_header = document.createElement('tr');
+        if(firstRow) {
+            let table_header = document.createElement('tr');
 
-        this.table.appendChild(table_header);
-
-
-        // Will be used over and over again for Header Cells
-        let th;
-        
-
-        // Manually add the Index column first
-        th = document.createElement('th');
-        th.className = 'recordtable_th';
-        th.innerHTML = 'index';
-
-        table_header.appendChild(th);
+            this.table.appendChild(table_header);
 
 
-        // Figure out what the Columns are by looking at the first Record
-        let columns = Object.keys(firstRow);
+            // Will be used over and over again for Header Cells
+            let th;
+            
 
-        // Add the Rest of the Columns
-        for(let i = 0; i < columns.length; i++) {
+            // Manually add the Index column first
             th = document.createElement('th');
             th.className = 'recordtable_th';
-
-            th.innerHTML = columns[i];
+            th.innerHTML = 'index';
 
             table_header.appendChild(th);
+
+
+            // Figure out what the Columns are by looking at the first Record
+            let columns = Object.keys(firstRow);
+
+            // Add the Rest of the Columns
+            for(let i = 0; i < columns.length; i++) {
+                th = document.createElement('th');
+                th.className = 'recordtable_th';
+
+                th.innerHTML = columns[i];
+
+                table_header.appendChild(th);
+            }
         }
     }
 
 
     append(data) {
-        // Will be used over and over again for each row of data
-        var tr;
-        var td;
-        var div;
+        if(data && data.length > 0) {
+            // Will be used over and over again for each row of data
+            var tr;
+            var td;
+            var div;
 
-        var columns = [];
-
-        if(data.length > 0) {
-            columns = Object.keys(data[0]);
-        }
+            var columns = Object.keys(data[0]);
 
 
-        for(let i = 0; i < data.length; i++) {
-            tr = document.createElement('tr');
-            this.table.appendChild(tr);
+            for(let i = 0; i < data.length; i++) {
+                tr = document.createElement('tr');
+                this.table.appendChild(tr);
 
 
-            // Add the Index
-            td = document.createElement('td');
-            td.className = 'recordtable_td';
-
-            div = document.createElement('div');
-            div.className = 'recordtable_data_container';
-
-            div.innerHTML = i;
-
-            tr.appendChild(td);
-            td.appendChild(div);
-
-
-            // Add the other columns for that row
-            for(let j = 0; j < columns.length; j++) {
+                // Add the Index
                 td = document.createElement('td');
                 td.className = 'recordtable_td';
 
                 div = document.createElement('div');
                 div.className = 'recordtable_data_container';
 
-                div.innerHTML = data[i][columns[j]];
+                div.innerHTML = i;
 
                 tr.appendChild(td);
                 td.appendChild(div);
+
+
+                // Add the other columns for that row
+                for(let j = 0; j < columns.length; j++) {
+                    td = document.createElement('td');
+                    td.className = 'recordtable_td';
+
+                    div = document.createElement('div');
+                    div.className = 'recordtable_data_container';
+
+                    div.innerHTML = data[i][columns[j]];
+
+                    tr.appendChild(td);
+                    td.appendChild(div);
+                }
             }
         }
     }
@@ -98,7 +98,7 @@ class RecordsTable {
     // Depth First Search through the DOMTree removing leaves as you go, lastChild first so that childNodes array doesn't have to shift
     _recursiveClear(root) {
         while(root.lastChild) {
-            this._remove_children(root.lastChild);
+            this._recursiveClear(root.lastChild);
 
             root.removeChild(root.lastChild);
         }
@@ -114,9 +114,9 @@ class RecordsTable {
 
         if(data && data.length > 0) {
             this._build_header(data[0]);
-        }
 
-        this.append(data);
+            this.append(data);
+        }
     }
 }
 
